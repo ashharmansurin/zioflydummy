97e374d154e34c7521eca04f59d7b97858d689b3
ashhar mansuri <ashharmansurin@gmail.com>
2021-05-24 12:46:18 +0530

    added slider
    added slider

--- js/main.js
+++ js/main.js
@@ -11,13 +11,22 @@ window.addEventListener('scroll', function (event) {
     }
 })
 
-// window.addEventListener('scroll', function (event) {
-//     let scroll = this.scrollY;
-//     console.log(scroll);
-//     // const header = document.querySelector('header');
-//     // if (scroll > 20) {
-//     //     header.classList.add('scroll-header');
-//     // } else {
-//     //     header.classList.remove('scroll-header');
-//     // }
-// });
+// recent added owl carousel slider jquery code
+
+$('.recent-added-slider').owlCarousel({
+    loop: true,
+    margin: 10,
+    stagePadding: 20,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 2
+        }
+    }
+})
